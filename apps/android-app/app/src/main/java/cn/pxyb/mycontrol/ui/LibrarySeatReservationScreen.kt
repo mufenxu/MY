31a37d486d64e02a506e03486c3bf27d2476cdce
mufenxu <mufenxu@users.noreply.github.com>
2026-08-29 21:44:21 +0800

    再次修复

--- apps/android-app/app/src/main/java/cn/pxyb/mycontrol/ui/LibrarySeatReservationScreen.kt
+++ apps/android-app/app/src/main/java/cn/pxyb/mycontrol/ui/LibrarySeatReservationScreen.kt
@@ -35,6 +35,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
